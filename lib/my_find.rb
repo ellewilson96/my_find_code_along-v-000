45adf6d3e4244d35052711d05beabba2680f2b37
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-14 18:31:05 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,9 @@ def my_find(collection)
   i = 0
   new_array = []
   while i < collection.length
-  new_array << yield(collection[i])
+  if yield(collection[i])
+    return collection [i]
+  end
   i = i + 1
 end
 end
